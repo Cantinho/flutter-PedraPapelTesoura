cc6e16fb3f521dc5e57adc790398a598463c1add
cesar-ferreira <cesar.ferreira.tn@gmail.com>
2019-10-08 23:44:42 -0300

    initial UI

---
+++ flutter_pedra_papel_tesoura/lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pedra_papel_tesoura/game.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Game(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
+
